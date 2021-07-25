9b2e53356d8568ba3d300fd99d4f8e54737f9043
Hoooooou-Jun <jipkim2@gmail.com>
2021-07-25 18:55:39 +0900

    (v 1.0.6_2)대대별 AI 검색 기능 추가

---
+++ src/kindsOfData/kindsOfSearchUnit.js
@@ -0,0 +1,9 @@
+export const KindsOfSearchUnit = [
+    { label: '전 제대', value: '2162'},
+    { label: '제97보병여단 1대대 (서천)', value: '97-1' },
+    { label: '제97보병여단 2대대 (보령)', value: '97-2' },
+    { label: '제98보병여단 1대대 (서산)', value: '98-1' },
+    { label: '제98보병여단 2대대 (당진)', value: '98-2' },
+    { label: '제98보병여단 3대대 (태안)', value: '98-3' },
+    { label: '제98보병여단 4대대 (홍성)', value: '98-4' },
+]
